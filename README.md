c5f7e69cd49a79e5301fabae962bbac306dadce9
Nankpak <nankpaktanfatanko@gmail.com>
2025-07-17 20:12:25 +0100

    second commit

---
+++ README.md
@@ -0,0 +1,9 @@
+This is how i spend my monthly allowance, 2000 Naira only.
+i spend 400 naira on buying book.
+i later found 100 naira on my pocket and i bought 250 naira snacks
+i gave 25% of my current allowance to my siblings
+and i used one-third of what is left for data then, i split the remaining 
+balance equally for saving and tithing.
+Finally i am left with 64 naira after removing all 100 naira note using the 
+modulus assignment operator
+
---
+++ allowance.py
@@ -0,0 +1,21 @@
+print("allowance is 2000 naira only")
+
+Allowance = 2000
+Allowance -= 400
+print(f"the remain balance after book was bought is {Allowance}")
+Allowance += 100
+print(f"after i found 100 naira the new balance is {Allowance}")
+Allowance -=250
+print(f"after spending of on snack the new balance is {Allowance}")
+percent = 25/100
+percent *= Allowance
+Allowance -= percent
+print("After 25% of used the allowance is", Allowance)
+onethird = 0.33
+onethird *= Allowance
+Allowance -= onethird
+print("After onethird been used the allowance become", Allowance)
+Allowance //=2
+print("After the allowance was equally divided for saving and tithe balance become", Allowance)
+Allowance %=100
+print("the remaining balance is now ", Allowance)
